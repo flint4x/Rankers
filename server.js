2feec34631659ea2c6005e9ea507b76ceca79bb4
flint4x <tyla4x@gmail.com>
2025-05-13 17:32:25 -0400

    Update server.js

--- server.js
+++ server.js
@@ -9,32 +9,30 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-// Parse form data
 app.use(express.urlencoded({ extended: true }));
 
-// Session middleware
 app.use(session({
   secret: 'supersecretkey',
   resave: false,
   saveUninitialized: false
 }));
 
-// Serve static files
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Store messages in memory
+const messageHistory = [];
 
 // Login route
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
-
   if (users[username] && users[username] === password) {
     req.session.user = username;
     return res.redirect('/chat.html');
   }
-
-  return res.redirect('/login.html'); // or optionally send error
+  return res.redirect('/login.html');
 });
 
-// Middleware to protect chat.html
+// Protect chat page
 app.get('/chat.html', (req, res, next) => {
   if (req.session.user) {
     return res.sendFile(path.join(__dirname, 'public', 'chat.html'));
@@ -43,13 +41,42 @@ app.get('/chat.html', (req, res, next) => {
   }
 });
 
-// Socket.io
+// Socket.io logic
+io.use((socket, next) => {
+  const session = socket.request.session;
+  if (session?.user) {
+    next();
+  } else {
+    next(new Error('Unauthorized'));
+  }
+});
+
 io.on('connection', (socket) => {
-  console.log('A user connected');
+  const username = socket.handshake.session.user;
+
+  // Send chat history to the newly connected user
+  socket.emit('chat history', messageHistory);
+
   socket.on('chat message', (msg) => {
-    io.emit('chat message', msg);
+    const fullMsg = { user: username, text: msg };
+    messageHistory.push(fullMsg);
+
+    // Limit message history (optional)
+    if (messageHistory.length > 100) {
+      messageHistory.shift();
+    }
+
+    io.emit('chat message', fullMsg);
   });
 });
+
+// Share sessions with socket.io
+const sharedSession = require('express-socket.io-session');
+io.use(sharedSession(session({
+  secret: 'supersecretkey',
+  resave: false,
+  saveUninitialized: false
+})));
 
 const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
