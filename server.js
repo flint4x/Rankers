bb2587ba053df6e0390d9247d3cc1ce0d13350e7
flint4x <tyla4x@gmail.com>
2025-05-13 17:28:08 -0400

    Update server.js

--- server.js
+++ server.js
@@ -1,38 +1,49 @@
 const express = require('express');
 const session = require('express-session');
 const http = require('http');
-const { Server } = require('socket.io');
 const path = require('path');
+const { Server } = require('socket.io');
 const users = require('./users');
 
 const app = express();
 const server = http.createServer(app);
 const io = new Server(server);
 
-app.use(express.static('public'));
+// Parse form data
 app.use(express.urlencoded({ extended: true }));
+
+// Session middleware
 app.use(session({
   secret: 'supersecretkey',
   resave: false,
-  saveUninitialized: true,
+  saveUninitialized: false
 }));
 
-// Login logic
+// Serve static files
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Login route
 app.post('/login', (req, res) => {
   const { username, password } = req.body;
+
   if (users[username] && users[username] === password) {
     req.session.user = username;
     return res.redirect('/chat.html');
   }
-  res.redirect('/login.html');
+
+  return res.redirect('/login.html'); // or optionally send error
 });
 
-// Middleware to protect chat page
-app.use('/chat.html', (req, res, next) => {
-  if (req.session.user) return next();
-  res.redirect('/login.html');
+// Middleware to protect chat.html
+app.get('/chat.html', (req, res, next) => {
+  if (req.session.user) {
+    return res.sendFile(path.join(__dirname, 'public', 'chat.html'));
+  } else {
+    return res.redirect('/login.html');
+  }
 });
 
+// Socket.io
 io.on('connection', (socket) => {
   console.log('A user connected');
   socket.on('chat message', (msg) => {
@@ -40,5 +51,5 @@ io.on('connection', (socket) => {
   });
 });
 
-const PORT = 3000;
-server.listen(PORT, () => console.log(`Server running at http://localhost:${PORT}`));
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
